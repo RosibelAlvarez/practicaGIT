b9b5340b60079ad0b8d8c974b94b89874fb9fc3a
Rosibel2594 <rossiel252594@gmail.com>
2022-12-07 14:52:32 +0100

    prueba

---
+++ index.js
@@ -0,0 +1,58 @@
+//hola mundo
+// console.log('hola mundo!');
+
+// //kata 2
+// var miNombre = "";
+// miNombre = prompt("ingrese su nombre");
+// console.log("Hola! " + miNombre);
+
+// //kata 3
+// let a = 10;
+// let b = 8;
+// console.log(a+b);
+
+// //kata 4
+// let a = 10;
+// console.log(a/2);
+
+// //kata 5
+// let a = 4;
+// let b = 6;
+// console.log(Math.max (a,b,));
+
+// //kata 6
+// let a = 3;
+// let b = 6;
+// let c= 9;
+// console.log (Math.max (a,b,c));
+
+// //kata 7
+// let a = 3;
+// let b = 6;
+// console.log( a===b);
+ 
+// //kata 8
+// let a = maria;
+// let b = fernanda;
+// console.log(a===b);
+
+// //kata 9
+// let num1= 4
+// let num2= -10
+
+// if((num1>0&&num2>0)||(num1<0&&num2<0)){
+//     console.log("True");
+// }else{
+//     console.log("False");
+// }
+
+
+
+let num1= 5
+let num2= -5
+
+if((num1>0&&num2<0)||(num1<0&&num2>0)){
+    console.log("True");
+}else{
+    console.log("False");
+}
